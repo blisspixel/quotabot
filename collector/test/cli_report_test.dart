bce9893a44dd1de3a66746c097d1b98c98773d08
Nick Seal <32712898+blisspixel@users.noreply.github.com>
2026-06-30 14:08:53 -0700

    Isolate stats CLI history test

--- collector/test/cli_report_test.dart
+++ collector/test/cli_report_test.dart
@@ -4,9 +4,20 @@ import 'dart:io';
 import 'package:test/test.dart';
 
 void main() {
+  late Directory temp;
+
+  setUp(() {
+    temp = Directory.systemTemp.createTempSync('quotabot_cli_report_');
+  });
+
+  tearDown(() {
+    if (temp.existsSync()) temp.deleteSync(recursive: true);
+  });
+
   Future<ProcessResult> runCli(List<String> args) {
     final env = Map<String, String>.from(Platform.environment)
       ..['QUOTABOT_DEMO'] = '1'
+      ..['LOCALAPPDATA'] = temp.path
       ..['NO_COLOR'] = '1';
     return Process.run(
       Platform.resolvedExecutable,
@@ -45,6 +56,16 @@ void main() {
   });
 
   test('stats prints sampled-day streaks', () async {
+    final cache = Directory('${temp.path}/quotabot/cache')
+      ..createSync(recursive: true);
+    final now = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+    final today = now - (now % Duration.secondsPerDay);
+    File('${cache.path}/buckets_codex.json').writeAsStringSync(jsonEncode([
+      _bucket(today - 2 * Duration.secondsPerDay, 80),
+      _bucket(today - Duration.secondsPerDay, 75),
+      _bucket(today, 70),
+    ]));
+
     final result = await runCli(['stats']);
 
     expect(result.exitCode, 0);
@@ -53,3 +74,14 @@ void main() {
     expect(output, contains('usable streak'));
   });
 }
+
+Map<String, dynamic> _bucket(int start, double headroom) => {
+      's': start - (start % Duration.secondsPerHour),
+      'n': 1,
+      'sum': headroom,
+      'sq': headroom * headroom,
+      'min': headroom,
+      'max': headroom,
+      'x': 0,
+      'h': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    };
